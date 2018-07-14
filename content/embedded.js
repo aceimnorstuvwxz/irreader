9388744d1fdfe028c9fa8aa41d989b46f6e2a923
fateleak <bf.chen@unicall.net.cn>
2018-07-15 00:08:02 +0800

    v1.1.1

--- content/embedded.js
+++ content/embedded.js
@@ -21,7 +21,7 @@ var g_targets = [
 
     {
         name: "V2EX-创意",
-        category: "二次元",
+        category: "社区",
         desc: "way to explore",
         icon: "http://fate2.oss-cn-shanghai.aliyuncs.com/feed-icons/vcy.png",
         link: "likefeed://add-remote?name=V2EX%20-%20%E5%88%9B%E6%84%8F&source=https%3A%2F%2Fwww.v2ex.com%2Ffeed%2Ftab%2Fcreative.xml&type=1&csser=&min_len=0&desc=way%20to%20explore&icon=http%3A%2F%2Ffate2.oss-cn-shanghai.aliyuncs.com%2Ffeed-icons%2Fvcy.png&charset=utf-8&config=%7B%22query_fields%22%3A%22*%22%2C%22unique_type%22%3A%220%22%7D"
@@ -33,13 +33,6 @@ var g_targets = [
         icon: "http://fate2.oss-cn-shanghai.aliyuncs.com/feed-icons/p.ico",
         link: "likefeed://add-remote?name=%5Bpixiv%5D%20%E7%BB%BC%E5%90%88%E6%9C%88%E6%A6%9C&source=https%3A%2F%2Fwww.pixiv.net%2Franking.php%3Fmode%3Dmonthly&type=0&csser=h2%20a&min_len=0&desc=P%E7%AB%99%E6%9C%88%E6%8E%92%E8%A1%8C%E6%A6%9C%E3%80%90%E5%B7%B2%E4%BF%AE%E5%A4%8D%E9%87%8D%E5%A4%8D%E3%80%91%E3%80%90%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91%E3%80%91&icon=https%3A%2F%2Fwww.pixiv.net%2Ffavicon.ico&charset=utf-8&config=%7B%22query_fields%22%3A%22illust_id%22%2C%22unique_type%22%3A%220%22%7D"
     },
-    // {
-    //     name: "二次元国家地理",
-    //     category: "二次元",
-    //     desc: "NGA论坛动漫版",
-    //     icon: "http://fate2.oss-cn-shanghai.aliyuncs.com/feed-icons/nga-b.png",
-    //     link: "likefeed://add-remote?name=%E4%BA%8C%E6%AC%A1%E5%85%83%E5%9B%BD%E5%AE%B6%E5%9C%B0%E7%90%86&source=http%3A%2F%2Fnga.178.com%2Fthread.php%3Ffid%3D-447601&type=0&csser=TD%5Bclass%3D%22c2%22%5D%3EA&min_len=0&desc=NGA%E5%8A%A8%E6%BC%AB%E8%AE%BA%E5%9D%9B&icon=http%3A%2F%2Ffate2.oss-cn-shanghai.aliyuncs.com%2Ffeed-icons%2Fnga-b.png&charset=utf-8&config=%7B%22query_fields%22%3A%22*%22%2C%22unique_type%22%3A%220%22%7D"
-    // },
     {
         name: "bilibili月度热排",
         category: "二次元",
@@ -136,7 +129,7 @@ var g_targets = [
 
 function init_recommend_targets() {
     //只放3个
-    g_targets.slice(0, 2).forEach((target) => {
+    g_targets.slice(0, 3).forEach((target) => {
         var new_ele = $('#target_template').clone()
         new_ele.removeAttr('id')
         new_ele.find('img').attr("src", target.icon)
