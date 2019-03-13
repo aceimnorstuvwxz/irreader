941099fa8d7934d23ea8bab9c2651d6d18231cc6
fateleak <bingfeng.ch3n@gmail.com>
2019-03-13 16:39:05 +0800

    op for gcd

--- content/embedded.js
+++ content/embedded.js
@@ -9,12 +9,6 @@ function get_query_param(name) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // var is_en = get_query_param('lan') == 'en'
-    // if (is_en) {
-    //     //如果是英文，则调到英文页面（用来mas review mother fucker!!!!)
-    //     window.location.href = 'embedded-en.html'
-    // }
-    //改成提供一个english的入口
     init_recommend_targets()
 
     $('.auto-ref').each(function(){
